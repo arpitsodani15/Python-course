6b092e099eac96b73334f99b9d10f36f1e260cb3
Arpit Sodani <sodaniarpit1522001@gmail.com>
2020-07-29 19:18:06 +0530

    Using Databases for Python Course

---
+++ Programs/Using Databases for Python/emailMessageDB.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+conn = sqlite3.connect('emailMessagesDB.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('CREATE TABLE Counts (org TEXT, count INTEGER)')
+
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox.txt'
+fh = open('mbox.txt')
+for line in fh:
+    line = line.strip()
+    if line.startswith('From:'):
+        pieces = line.split()
+        email = pieces[1]
+        emailPieces = email.split('@')
+        org = emailPieces[1]
+        cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+        row = cur.fetchone()
+        if row is None:
+            cur.execute('''INSERT INTO Counts (org, count) VALUES (?, 1)''', (org,))
+        else:
+            cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',(org,))
+
+conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
